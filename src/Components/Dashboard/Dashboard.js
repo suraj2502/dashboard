20c1b56454554ab3991fbcbe250f33eaafb58979
suraj2502 <shuklasuraj620@gmail.com>
2020-07-03 16:30:08 +0530

    third commit

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,13 +1,13 @@
 
 import * as React from "react";
 import { Admin, Resource,EditGuesser } from 'react-admin';
-import { employeeList } from '../employeeList';
+import { employeeList,PostEdit,PostCreate } from '../employeeList';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const Dashboard = () => (
     <Admin  dataProvider={dataProvider}>
-        <Resource name="Users" list={employeeList}  edit={EditGuesser} />
+        <Resource name="Users" list={employeeList}  edit={PostEdit} create={PostCreate} />
     </Admin>
 );
 
--- src/Components/employeeList.js
+++ src/Components/employeeList.js
@@ -1,16 +1,40 @@
 import * as React from "react";
-import { List, Datagrid, TextField, EmailField } from 'react-admin';
+import { List, Datagrid, TextField, EmailField,TextInput,Edit,SimpleForm ,Create} from 'react-admin';
 export const employeeList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
           <TextField source="id" />
             <TextField source="name" />
--           <TextField source="username" />
+-           <TextField source="age" />
             <EmailField source="email" />
--           <TextField source="address.street" />
             <TextField source="phone" />
-            <TextField source="website" />
+-           <TextField source="address.street" />
             <TextField source="company.name" />
         </Datagrid>
     </List>
+);
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+           <TextInput source="id" />
+           <TextInput source="name" />
+            <TextInput source="age" />
+           <TextInput source="phone" />
+         <TextInput multiline source="email" />
+        </SimpleForm>
+    </Edit>
+);
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+        <TextInput disabled source="id" />
+            
+                       <TextInput source="name" />
+                        <TextInput source="age" />
+                       <TextInput source="phoneNo" />
+                     <TextInput multiline source="email" />
+                    
+        </SimpleForm>
+    </Create>
 );
